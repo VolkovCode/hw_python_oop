91ed7c95f8571c831baa550ebb4ae543eb5497e3
VolkovCode <59123114+VolkovCode@users.noreply.github.com>
2020-01-18 12:47:46 +0300

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,7 +1,6 @@
 import datetime as dt
+
 date_format = '%d.%m.%Y'
-
-
 
 class Calculator:
     def __init__(self, limit):
@@ -10,7 +9,6 @@ class Calculator:
 
     def add_record(self, record):
         self.records.append(record)
-       
 
     def get_today_stats(self):
         count = 0
@@ -19,17 +17,15 @@ class Calculator:
             if record.date == today:
                 count += record.amount        
         return count
-        
+    
     def get_week_stats(self):
         weekly_sum = 0
         day_week_ago = dt.datetime.today().date() - dt.timedelta(days=7)
         for record in self.records:
             if day_week_ago <= record.date: 
                 weekly_sum += record.amount 
-        return(weekly_sum)       
+        return weekly_sum       
 
-
-    pass
 
 class CaloriesCalculator(Calculator):
     
@@ -41,49 +37,41 @@ class CaloriesCalculator(Calculator):
         else:
             return 'Хватит есть!' 
 
+
 class CashCalculator(Calculator):  
     USD_RATE = 60.00
     EURO_RATE = 70.00
 
     def get_today_cash_remained(self, currency):
         balance = self.limit - self.get_today_stats()
-        balanceUSD = round((balance / self.USD_RATE), 2)
-        balanceEUR = round((balance / self.EURO_RATE), 2)
+        balance_USD = round((balance / self.USD_RATE), 2)
+        balance_EUR = round((balance / self.EURO_RATE), 2)
         
         if self.get_today_stats() == self.limit:
             return 'Денег нет, держись'
-
-        if currency == 'rub':
-
-            if self.get_today_stats() < self.limit:
+        
+        elif self.get_today_stats() < self.limit:
+            if currency == "rub":
                 return f'На сегодня осталось {balance} руб'
-            elif self.get_today_stats() > self.limit:
+            elif currency == "eur":
+                return f'На сегодня осталось {balance_EUR} Euro'
+            elif currency == "usd":
+                return f'На сегодня осталось {balance_USD} USD'
+
+        elif self.get_today_stats() > self.limit:
+            if currency == "rub":
                 return f'Денег нет, держись: твой долг - {abs(balance) } руб'
-            elif balance == 0:
-                return 'Денег нет, держись'    
-        
-        elif currency == "usd":    
-            
-            if self.get_today_stats() < self.limit:
-                return f'На сегодня осталось {balanceUSD} USD'
-            elif self.get_today_stats() > self.limit:
-                return f'Денег нет, держись: твой долг - {abs(balanceUSD)} USD'
-        elif currency == "eur":   
-            if self.get_today_stats() < self.limit:
-                return f'На сегодня осталось {balanceEUR } Euro'
-            elif self.get_today_stats() > self.limit:
-                return f'Денег нет, держись: твой долг - {abs(balanceEUR)} Euro'
-        
-         
+            elif currency == "eur":
+                return f'Денег нет, держись: твой долг - {abs(balance_EUR)} Euro'
+            elif currency == "usd":
+                return f'Денег нет, держись: твой долг - {abs(balance_USD)} USD'
+                
 
 class Record:
-    def __init__(self, amount, comment, date = None):
+    def __init__(self, amount, comment, date=None):
         self.amount = int(amount)     
         self.comment = comment
         if date:
             self.date = dt.datetime.strptime(date, date_format).date()
         else:
-            self.date = dt.datetime.now().date()   
-   
-
-    pass
+            self.date = dt.datetime.now().date()      
